1148181293972949561e2636d44dae610921aa4e
Zeeshan Nawaz <127197425+zeshannawaz@users.noreply.github.com>
2025-11-13 22:26:51 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import Header from './Components/Header';
+import Hero from './Components/Hero';
+import Services from './Components/Services';
+import Portfolio from './Components/Portfolio';
+import Testimonial from './Components/Testimonial';
+import CTA from './Components/CTA';
+import Footer from './Components/Footer';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Services />
+      <Portfolio />
+      <Testimonial />
+      <CTA />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
